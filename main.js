712890bd287ff582a9167faced75923a4490c033
omar elsehrawy <mero20261@gmail.com>
2020-02-24 18:02:28 +0200

    added frontend to the project

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     app = express();
+    const cors = require('cors');
+    const path = require('path');
+
+
 var {AddItem,getItems,updateItem,deleteItem} = require("./customers/customers.controller")
 var db = require('mongoose')
 db.connect('mongodb+srv://omar:zBWusbyYPjOlBpia@cluster0-utyhh.mongodb.net/test?retryWrites=true&w=majority')
-app.use(bodyParser.urlencoded())
 
-app.get('/',(req, res)=>{  
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json({limit: '50mb'}));
+
+
+app.use(cors());
+
+/* app.get('/',(req, res)=>{  
    res.send('<h1> Hello World 2 </h1>');  
-});
+ });*/
 
 app.get('/customers',getItems);
 
@@ -17,5 +26,10 @@ app.post('/customers',AddItem);
 app.put('/customers/:id',updateItem);
 
 app.delete('/customers/:id',deleteItem);
+
+/*------ front-end / Set Static Folder-------*/
+app.use(express.static(path.join(__dirname,'public')));
+app.get('*',(req,res) => { res.sendFile(path.join(__dirname,'public/index.html')) });
+/*-----------------------------*/
 
 app.listen('3000',() => {  console.log('server started on port 3000'); });
